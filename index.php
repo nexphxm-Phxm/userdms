72acdd063a97a51648c8293c864b8b60954dad07
durov <nexphxm@gmail.com>
2026-07-19 10:05:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 // ==========================================
 // 1. HARDCODED CONFIG
 // ==========================================
+// 1. HARDCODED CONFIG
+// ==========================================
 $botToken = "8814977950:AAEr7T-rHx3jE8Dj7zEKmsszORCUBy3_vF4"; 
 $defaultImage = "https://t.me/NEXm2m/824"; 
-$defaultAdmin = "5157557268";
-$secondAdmin = "7177448473"; // Add second admin here
-
+$admins = ["5157557268", "7177448473"];
 $defaultChannels = [];
 $defaultFolders = [];
 $defaultSolvedPost = "https://t.me/NEXm2m/861";
